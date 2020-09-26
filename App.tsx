4832962b854b0340276370ce40d482d0b53be236
Manuel Betancurt <m.betancurt@wisr.com.au>
2020-09-27 03:30:35 +1000

    prima nocte

---
+++ App.tsx
@@ -0,0 +1,29 @@
+
+import * as React from 'react';
+import { Button, View, Text } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+//
+import HomeScreen from './source/screens/HomeScreen'
+import DetailsScreen from './source/screens/DetailScreen'
+
+
+
+
+
+
+const Stack = createStackNavigator();
+
+function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Details" component={DetailsScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+export default App;
---
+++ source/screens/DetailScreen.tsx
@@ -0,0 +1,14 @@
+
+import * as React from 'react';
+import { Button, View, Text } from 'react-native';
+
+function DetailsScreen({ navigation }) {
+    return (
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        <Text>Details Screen ss9</Text>
+        
+      </View>
+    );
+  }
+
+  export default DetailsScreen;
