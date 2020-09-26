118b56a8d9b5298d3017486bf42b25e883d8b69b
Manuel Betancurt <m.betancurt@wisr.com.au>
2020-09-27 03:39:24 +1000

    table added

--- source/screens/HomeScreen.tsx
+++ source/screens/HomeScreen.tsx
@@ -1,18 +1,73 @@
 
-import * as React from 'react';
-import { Button, View, Text } from 'react-native';
+import React , { useState } from 'react';
+import { FlatList, SafeAreaView, Text, TouchableOpacity, StyleSheet, StatusBar } from 'react-native';
+
+
+
+const DATA = [
+    {
+      id: "bd7acbea-c1b1-46c2-aed5-3ad53abb28ba",
+      title: "First Item",
+    },
+    {
+      id: "3ac68afc-c605-48d3-a4f8-fbd91aa97f63",
+      title: "Second Item",
+    },
+    {
+      id: "58694a0f-3da1-471f-bd96-145571e29d72",
+      title: "Third Item",
+    },
+  ];
+  
+  const Item = ({ item, onPress, style }) => (
+    <TouchableOpacity onPress={onPress} style={[styles.item, style]}>
+      <Text style={styles.title}>{item.title}</Text>
+    </TouchableOpacity>
+  );
 
 
 function HomeScreen({ navigation }) {
+
+
+    const [selectedId, setSelectedId] = useState(null);
+    
+    const renderItem = ({ item }) => {
+        const backgroundColor = item.id === selectedId ? "#6e3b6e" : "#f9c2ff";
+    
+        return (
+          <Item
+            item={item}
+            onPress={() => setSelectedId(item.id)}
+            style={{ backgroundColor }}
+          />
+        );
+      };
+
     return (
-      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-        <Text>Home Screen mako</Text>
-        <Button
-          title="Go to Details"
-          onPress={() => navigation.navigate('Details')}
+        <SafeAreaView style={styles.container}>
+        <FlatList
+          data={DATA}
+          renderItem={renderItem}
+          keyExtractor={(item) => item.id}
+          extraData={selectedId}
         />
-      </View>
+      </SafeAreaView>
     );
   }
+
+  const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      marginTop: StatusBar.currentHeight || 0,
+    },
+    item: {
+      padding: 20,
+      marginVertical: 8,
+      marginHorizontal: 16,
+    },
+    title: {
+      fontSize: 32,
+    },
+  });
 
   export default HomeScreen;
